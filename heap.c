c59cd44c402749dd5cb84058095d78de305f4746
LotaSchwager <>
2022-05-05 03:36:07 +0000

    new try

--- heap.c
+++ heap.c
@@ -60,6 +60,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
   pq->heapArray[pq->size].priority = priority;
   pq->heapArray[pq->size].data = data;
+  pq->size++;
   subir (pq, priority);
 }
 
