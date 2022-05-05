620cacf3b20e97fb9c1360b16c8e7891f40edad4
LotaSchwager <>
2022-05-05 01:45:46 +0000

    new try

--- heap.c
+++ heap.c
@@ -40,6 +40,7 @@ void heap_pop(Heap* pq){
   pq->heapArray[0].data = pq->heapArray[pq->size-1].data;
   pq->heapArray[0].priority = pq->heapArray[pq->size-1].priority;
   pq->heapArray[pq->size-1].data = NULL;
+  pq->heapArray[pq->size-1].data = 0;
   pq->size--;
 
   if (pq->heapArray[1].priority > pq->heapArray[2].priority){
